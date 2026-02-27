9ec6874d5a0535cecceb6f7e2e534449be1f581b
Arun Baburaj <arunbaburaj@Mac-mini.local>
2026-02-27 11:19:48 +0530

    Fix dark mode overriding on case study pagesv2

--- src/Specter.jsx
+++ src/Specter.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Specter.css';
+//import './Specter.css';
 
 export default function Specter() {
   useEffect(() => {
